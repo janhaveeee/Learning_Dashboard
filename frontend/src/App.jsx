e50a2f17187d78cd03ba1f10329a71f4c03cf2d2
Janhavi <janhavirevdekar@gmail.com>
2025-03-31 16:00:19 +0530

    final wrok done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import "./index.css"
 function App() {
   return (
     <div>
-      <h1>Learning Dashboard</h1>
+      <h1>🤖LearnTrack- AI based Learning Dashboard</h1>
       <PredictionForm />
     </div>
   );
